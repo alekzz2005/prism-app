9a9d28713494c3c1d1f89b6d9e722a00fbff970e
alekzz2005 <binagatanalexander2005@gmail.com>
2026-05-17 02:11:40 +0800

    fix(auth): restore build.gradle.kts to avoid teammate conflicts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 android {
     namespace = "com.example.prism_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
